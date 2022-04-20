c6779c0d394262ae52e66c7a6cdca38378dbe203
AlthafHussainK <althafakm@gmail.com>
2022-04-20 16:35:10 +0530

    createStrore implemetation added

--- script.js
+++ script.js
@@ -9,7 +9,33 @@ const counter = (state = 0, action) => {
   }
 }
 
-const { createStore } = Redux;
+// const { createStore } = Redux; -> createStore Implementation below
+
+const createStore = (reducer) => {
+  let state;
+  let listeners = []
+
+  const getState = () => state
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  };
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+    };
+  };
+
+  dispatch({});
+
+  return { getState, dispatch, subscribe };
+};
+
+// <-- 
+
 const store = createStore(counter);
 
 const render = () => {
